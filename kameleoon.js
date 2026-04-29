c229cb6bdb16f0d4934f11d4e1b5dd44462b4c88
ezheenchoroev <122873039+ezheenchoroev@users.noreply.github.com>
2026-04-29 13:07:30 -0300

    Update kameleoon.js

--- kameleoon.js
+++ kameleoon.js
@@ -33,7 +33,7 @@ if (!document.getElementById("kameleoonLoadingStyleSheet") && !window.kameleoonD
 
 var kameleoonScript = document.createElement("script");
 kameleoonScript.type = "text/javascript";
-kameleoonScript.src = "//okrwie2k2z.experimentation.workforce.staging.principal.com/kameleoon.js"; // dev
+kameleoonScript.src = "//675nqkgcoe.experimentation.workforce.dev.principal.com/kameleoon.js"; // dev
 // kameleoonScript.src = "//dddja6qs8f.kameleoon.brightspot.com/kameleoon.js";
 kameleoonScript.async = true;
 kameleoonScript.setAttribute("fetchpriority", "high");
